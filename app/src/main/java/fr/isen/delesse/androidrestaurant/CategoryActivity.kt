2f2ca3d10eb6aeced527baff199017aab335ef84
Alexandre <alex.delesse01@gmail.com>
2021-01-25 16:50:28 +0100

    part 3 - a OKAY

--- app/src/main/java/fr/isen/delesse/androidrestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/delesse/androidrestaurant/CategoryActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import fr.isen.delesse.androidrestaurant.databinding.ActivityCategoryBinding
 
 class CategoryActivity : AppCompatActivity() {
 
@@ -11,10 +12,27 @@ class CategoryActivity : AppCompatActivity() {
         ENTREES, PLATS, DESSERTS
     }
 
+    private lateinit var binding : ActivityCategoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("CategoryActivity", "start of CategoryActivity")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
+        Log.d("CategoryActivity", "start of CategoryActivity")
+        binding = ActivityCategoryBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_category)
+        setContentView(binding.root)
+
+        val selectedItem = intent.getSerializableExtra(HomeActivity.CATEGORY_NAME) as? ItemType
+        Log.d("test", getCategoryTitle(selectedItem))
+        binding.categoryTitle.text = getCategoryTitle(selectedItem)
+
+    }
+    private fun getCategoryTitle( item: ItemType?): String {
+        return when(item) {
+            ItemType.ENTREES -> getString(R.string.app_entree)
+            ItemType.PLATS -> getString(R.string.app_plat)
+            ItemType.DESSERTS -> getString(R.string.app_dessert)
+            else -> ""
+        }
     }
 
     override fun onDestroy() {
