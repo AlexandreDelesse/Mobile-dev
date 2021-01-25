4b951f9f8ef0ac44508ecaf09c40f12427c578be
Alexandre <alex.delesse01@gmail.com>
2021-01-25 11:59:08 +0100

    Homepage design
    added :
    - 3 buttons ( entree plat dessert )
    - title text
    - img

--- app/src/main/java/fr/isen/delesse/androidrestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/delesse/androidrestaurant/HomeActivity.kt
@@ -2,11 +2,25 @@ package fr.isen.delesse.androidrestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.delesse.androidrestaurant.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonPlat.setOnClickListener {
+            
+        }
         setContentView(R.layout.activity_home)
-        // test commentaire
+
+
+
+
     }
 }
